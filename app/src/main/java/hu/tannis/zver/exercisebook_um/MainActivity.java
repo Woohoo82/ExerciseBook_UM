ae2aa6668e395e96d36bf038f5831a3a8b5698ff
Zoltán Vér <vzolko@gmail.com>
2017-10-05 17:20:42 +0200

    Add new menu entries

--- app/src/main/java/hu/tannis/zver/exercisebook_um/MainActivity.java
+++ app/src/main/java/hu/tannis/zver/exercisebook_um/MainActivity.java
@@ -1,10 +1,13 @@
 package hu.tannis.zver.exercisebook_um;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
         setContentView(R.layout.activity_main);
     }
 
@@ -29,5 +33,27 @@ public class MainActivity extends AppCompatActivity {
     public void openChorder(View view) {
         Intent i = new Intent(this, ChorderActivity.class);
         startActivity(i);
+    }
+
+    public void openMetronome(View view) {
+        //Intent i = new Intent(this, MetronomeActivity.class);
+        //startActivity(i);
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, R.string.message_na, duration);
+        toast.show();
+    }
+
+    public void openEarTrainer(View view) {
+        //Intent i = new Intent(this, EarTrainerActivity.class);
+        //startActivity(i);
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, R.string.message_na, duration);
+        toast.show();
     }
 }
